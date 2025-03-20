ce39394e698c5a66c896b267156e189d78338a42
Santt4na <jhonnsantt4na@gmail.com>
2025-03-20 20:34:49 -0300

    feat: initialize project and apply layered architecture

---
+++ src/main/java/com/santt4na/rapidinner/RapidinnerApplication.java
@@ -0,0 +1,13 @@
+package com.santt4na.rapidinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santt4na/rapidinner/RapidinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4na.rapidinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
